1142791d2dda675260dd894d2157331117b22617
Irakli <ir.mchedlishvili@gmail.com>
2016-03-25 21:54:23 +0400

    process.py and README.md added for continent codes.

---
+++ scripts/process.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import os
+import csv
+
+CONTINENTS = {'AF': 'Africa',
+				'AN':'Antarctica',
+				'AS': 'Asia',
+				'EU': 'Europe',
+				'NA': 'North America',
+				'OC': 'Oceania',
+				'SA': 'South America'}
+				
+
+def process():
+	'''
+	
+	'''
+	filename = 'continent_codes'
+	if not os.path.exists('data'):
+		os.makedirs('data')
+	with open('data/' + filename, 'w') as working_file:
+		csvwriter = csv.writer(working_file)
+		header = ['Code','Name']
+		for continent in CONTINENTS:
+			csvwriter.writerow([continent, CONTINENTS[continent]])
+						
+if __name__ == '__main__':
+	process()
